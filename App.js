49ca3cdec6211ec203ad2d8c8843113428e1165b
hyejin <nhj1043@naver.com>
2020-07-30 16:17:45 +0900

    Refactorting: Signup 모듈 테스트 할 코드와 파일을 추가했습니다

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 // import { StatusBar } from 'expo-status-bar';
-import React from "react";
+import React from 'react';
 
-import Main from "./screens/Main";
-import Reservation from "../OMM-client/Component/Reservation";
+// import Main from "./screens/MainScreen";
+import Signup from './screens/SignupScreen';
+// import Reservation from "../OMM-client/Component/Reservation";
 
 // App 메인화면
 export default function App() {
   // session이 유지가 안 되어 있으면 Main 화면으로 return
-  return <Main />;
+  return <Signup />;
 }
