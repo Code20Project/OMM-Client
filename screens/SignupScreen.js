1c701cd6e4777267b4e6ed3eb53b9e1725396f70
hyejin <nhj1043@naver.com>
2020-08-01 01:15:53 +0900

    Feat: 가입 버튼 활성화 관련된 기능 추가했습니다.
    - inputData를 서버로 보내는 작업 코드 작성할 예정
    - modal 관련해서 안내 창 띄우는 기능 추가 예정

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,166 +1,223 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
-  StyleSheet, View, Text, TextInput, Button,
+  StyleSheet, View, Text, TextInput, Button, Alert, Platform,
 } from 'react-native';
 import { RadioButton } from 'react-native-paper';
+
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Constants from 'expo-constants';
 
 import signupStyle from '../styles/signupStyle';
 
 const styles = StyleSheet.create(signupStyle);
 
 export default function Main() {
-  // state
-  const [state, setState] = useState({
-    mentee_name: '',
-    email: '',
-    password: '',
-    sex: '남',
-    phone: '',
-    birthday: '',
-  });
-
+  // 값들을 셋팅하는 함수
+  const [mentee_name, setMentee_name] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [confirmPassword, setComfirmPassword] = useState('');
+  const [selectedSex, setSelectedSex] = useState('남');
+  const [phone, setPhone] = useState('');
+  const [birthday, setBirthday] = useState('');
 
-  const setSexHandler = (value) => {
-    setState(
-      { sex: value },
-    );
+  const setValueHandler = (key, value) => {
+    switch (key) {
+      case 'mentee_name': setMentee_name(value);
+        break;
+      case 'email': setEmail(value);
+        break;
+      case 'password': setPassword(value);
+        break;
+      case 'confirmPassword': setComfirmPassword(value);
+        break;
+      case 'selectedSex': setSelectedSex(value);
+        break;
+      case 'phone': setPhone(value);
+        break;
+      case 'birthday': setBirthday(value);
+        break;
+    }
   };
 
-  const onChangeText = (text) => {
-    setState(
-      { password: text },
-    );
+  // 서버에 보내줄 정보
+  const inputDataObj = {
+    mentee_name,
+    email,
+    password,
+    sex: selectedSex,
+    phone,
+    birthday,
   };
 
+  let checkPassword = false;
   const checkPasswordHandler = () => {
-    if (confirmPassword.length !== 0 && confirmPassword !== state.password) { // password가 일치 하지 않으면
+    if (confirmPassword.length !== 0 && confirmPassword !== password) { // password가 일치 하지 않으면
       return (
         <View>
           <TextInput
             style={styles.checkPassword}
             placeholder="********"
             secureTextEntry // 입력한 테스트를 암호화 해주는 속성
-            onChangeText={(text) => setComfirmPassword(text)}
+            onChangeText={(text) => setValueHandler('confirmPassword', text)}
             value={confirmPassword}
           />
           <Text style={{ fontSize: 10, marginLeft: 20, color: 'red' }}>패스워드가 일치하지 않습니다.</Text>
         </View>
       );
     }
-
+    checkPassword = true;
     return (
       <View>
         <TextInput
           style={styles.textInput}
           placeholder="********"
           secureTextEntry // 입력한 테스트를 암호화 해주는 속성
-          onChangeText={(text) => setComfirmPassword(text)}
+          onChangeText={(text) => setValueHandler('confirmPassword', text)}
           value={confirmPassword}
         />
       </View>
     );
   };
 
-  useEffect(() => { // 랜더링이 끝나면 useEffect 훅에 입력된 함수가 호출된다.
+  const setButton = () => { // button의 상태를 결정하는 함수
+    let checkInputValue = true;
+    for (const i in inputDataObj) {
+      if (inputDataObj[i].length === 0) {
+        checkInputValue = false;
+      }
+    }
 
-  });
+    // 비밀번호확인까지 채워져야 한다.
+    if (confirmPassword.length === 0) {
+      checkInputValue = false;
+    }
+
+    // checkpassword true, checkInputValue true => buttonState false
+    const buttonState = !(checkInputValue && checkPassword);
+    return (
+      <Button
+        title="가입"
+        color="#488888"
+        disabled={buttonState} // boolean function 작성
+    // onPress 이벤트가 발생했을 경우에 서버로 value들을 전송하는 작업이 일어나야 한다.
+    // 또한 성공적으로 가입했으면 로그인 화면으로, 성공적으로 가입이 되지 않았으면 경고 메세지를 띄운다.
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+    );
+  };
+
+  // useEffect(() => { // 랜더링이 끝나면 useEffect 훅에 입력된 함수가 호출된다.
+
+  // });
 
   return (
-    <View style={styles.container}>
-      <View style={styles.textfield}>
-        <View style={styles.text}>
-          <Text>이름</Text>
-        </View>
-        <View>
-          <TextInput
-            style={styles.textInput}
-            placeholder="홍길동"
-          />
-        </View>
-      </View>
-      <View style={styles.textfield}>
-        <View style={styles.text}>
-          <Text>아이디(이메일)</Text>
-        </View>
-        <View>
-          <TextInput
-            style={styles.textInput}
-            placeholder="ID@email.com"
-          />
-        </View>
-      </View>
-      <View style={styles.textfield}>
-        <View style={styles.text}>
-          <Text>비밀번호</Text>
-        </View>
-        <View>
-          <TextInput
-            style={styles.textInput}
-            placeholder="********"
-            secureTextEntry // 입력한 테스트를 암호화 해주는 속성
-            onChangeText={(text) => onChangeText(text)}
-          />
-        </View>
-      </View>
-      <View style={styles.textfield}>
-        <View classname="checkPassword" style={styles.text}>
-          <Text>비밀번호 확인</Text>
-        </View>
-        {checkPasswordHandler()}
-      </View>
-      <View style={styles.textfield}>
-        <View style={styles.text}>
-          <Text>휴대폰 번호</Text>
-        </View>
-        <View>
-          <TextInput
-            style={styles.textInput}
-            placeholder="010-0000-0000"
-          />
-        </View>
-      </View>
-      <View style={styles.textfield}>
-        <View style={styles.text}>
-          <Text>생년월일</Text>
-        </View>
-        <View>
-          <TextInput
-            style={styles.textInput}
-            placeholder="000000(6자리)"
-          />
-        </View>
-      </View>
-      <View style={styles.radios}>
-        <View style={{ width: '37%', marginTop: 5 }}>
-          <Text>성별</Text>
-        </View>
-        <RadioButton.Group onValueChange={(value) => setSexHandler(value)} value={state.sex}>
-          <View style={{ flexDirection: 'row' }}>
-            <RadioButton
-              value="남"
-              color="#488888"
-            />
-            <Text style={styles.radioText}>남</Text>
+    <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+      <View style={{ flex: 1 }} />
+      <KeyboardAwareScrollView>
+        <View style={styles.container}>
+          <View style={styles.textfield}>
+            <View style={styles.text}>
+              <Text>이름</Text>
+            </View>
+            <View>
+              <TextInput
+                style={styles.textInput}
+                placeholder="홍길동"
+                onChangeText={(text) => setValueHandler('mentee_name', text)}
+              />
+            </View>
           </View>
-          <View style={{ flexDirection: 'row', marginLeft: 20 }}>
-            <RadioButton
-              value="여"
-              color="#488888"
-            />
-            <Text style={styles.radioText}>여</Text>
+          <View style={styles.textfield}>
+            <View style={styles.text}>
+              <Text>아이디(이메일)</Text>
+            </View>
+            <View>
+              <TextInput
+                style={styles.textInput}
+                placeholder="ID@email.com"
+                onChangeText={(text) => setValueHandler('email', text)}
+              />
+            </View>
           </View>
-        </RadioButton.Group>
-      </View>
-      <View style={styles.button}>
-        <Button
+          <View style={styles.textfield}>
+            <View style={styles.text}>
+              <Text>비밀번호</Text>
+            </View>
+            <View>
+              <TextInput
+                style={styles.textInput}
+                placeholder="********"
+                secureTextEntry // 입력한 테스트를 암호화 해주는 속성
+                onChangeText={(text) => setValueHandler('password', text)}
+              />
+            </View>
+          </View>
+          <View style={styles.textfield}>
+            <View classname="checkPassword" style={styles.text}>
+              <Text>비밀번호 확인</Text>
+            </View>
+            {checkPasswordHandler()}
+          </View>
+          <View style={styles.textfield}>
+            <View style={styles.text}>
+              <Text>휴대폰 번호</Text>
+            </View>
+            <View>
+              <TextInput
+                style={styles.textInput}
+                placeholder="01000000000"
+                onChangeText={(text) => setValueHandler('phone', text)}
+              />
+            </View>
+          </View>
+          <View style={styles.textfield}>
+            <View style={styles.text}>
+              <Text>생년월일</Text>
+            </View>
+            <View>
+              <TextInput
+                style={styles.textInput}
+                placeholder="000000(6자리)"
+                onChangeText={(text) => setValueHandler('birthday', text)}
+              />
+            </View>
+          </View>
+          <View style={styles.radios}>
+            <View style={{ width: '37%', marginTop: 5 }}>
+              <Text>성별</Text>
+            </View>
+            <RadioButton.Group onValueChange={(value) => setValueHandler('selectedSex', value)} value={selectedSex}>
+              <View style={{ flexDirection: 'row' }}>
+                <RadioButton
+                  value="남"
+                  color="#488888"
+                />
+                <Text style={styles.radioText}>남</Text>
+              </View>
+              <View style={{ flexDirection: 'row', marginLeft: 20 }}>
+                <RadioButton
+                  value="여"
+                  color="#488888"
+                />
+                <Text style={styles.radioText}>여</Text>
+              </View>
+            </RadioButton.Group>
+          </View>
+          <View style={styles.button}>
+            {/* <Button
           title="가입"
           color="#488888"
-          disabled={false}
+          disabled={setButton()} // boolean function 작성
           // onPress 이벤트가 발생했을 경우에 서버로 value들을 전송하는 작업이 일어나야 한다.
-          //  onPress={() => Alert.alert('Simple Button pressed')}
-        />
-      </View>
+          // 또한 성공적으로 가입했으면 로그인 화면으로, 성공적으로 가입이 되지 않았으면 경고 메세지를 띄운다.
+            // onPress={() => console.log('onPress')}
+            // Alert.alert('Simple Button pressed')
+        /> */}
+            {setButton()}
+          </View>
+        </View>
+      </KeyboardAwareScrollView>
     </View>
   );
 }
