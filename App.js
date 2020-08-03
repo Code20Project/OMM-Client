30636f8886c03a8a03660ec3ed89c3b0bb4e5ee5
hyejin <nhj1043@naver.com>
2020-08-03 20:36:39 +0900

    Refactor: MainScreen code를 다시 리펙토링 했습니다.
    - 버튼이벤트에 따라 mentor, mentee Login 화면 연동 예정

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
-// import Main from "./screens/MainScreen";
+import Main from './screens/MainScreen';
 // import Signup from './screens/SignupScreen';
-import Login from './screens/LoginScreen';
+// import Login from './screens/LoginScreen';
 // import Reservation from "../OMM-client/Component/Reservation";
 
 // App 메인화면
 export default function App() {
   // session이 유지가 안 되어 있으면 Main 화면으로 return
-  return <Login />;
+  return <Main />;
 }
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,35 +1,47 @@
 import React from 'react';
 import {
-  StyleSheet, View, Text, Button,
+  StyleSheet, View, Text, Button, ImageBackground, Platform,
 } from 'react-native';
+
+import Constants from 'expo-constants';
 
 import mainStyle from '../styles/mainStyle';
 
 const styles = StyleSheet.create(mainStyle);
 
+const image = require('../imgs/mainImg.jpg');
+
 export default function Main() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>
-          OOM
-        </Text>
-      </View>
+    <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+      <ImageBackground source={image} style={styles.image}>
+        <View style={styles.overlay}>
+          <View style={styles.container}>
+            <Text style={styles.text}>
+              OOM
+            </Text>
+          </View>
 
-      <View>
-        <Button
-          onpress={() => {
-            console.log('test');
-          }}
-          title="Mentor"
-        />
-        <Button
-          onpress={() => {
-            console.log('test');
-          }}
-          title="Mentee"
-        />
-      </View>
+          <View style={{ flex: 1 }}>
+            <View style={styles.button}>
+              <Button
+                onpress={() => {
+                  console.log('test');
+                }}
+                title="Mentor"
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                onpress={() => {
+                  console.log('test');
+                }}
+                title="Mentee"
+              />
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
