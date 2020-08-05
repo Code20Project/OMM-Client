be585c284922378c53757ab1b86f4a510f5e5a4c
choisungyu <maroon92@naver.com>
2020-08-06 00:42:09 +0900

    Feat: HomeScreen 페이지 리스트들 완성했습니다.
    - FlatList로 가로로는 신규강의,추천강의,인기강의 만들었습니다
    - 세로르는 ScrollView로 스크롤 되게 했습니다

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,60 @@
 import React, { useState } from 'react';
 
-import { View } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 
+import { ScrollView } from 'react-native-gesture-handler';
 import NewLectureScreen from './NewLectureScreen';
+import RecommendationLectureScreen from './RecommendationLectureScreen';
+import PopularLectureScreen from './PopularLectureScreen';
 
 export default function HomeScreen() {
   return (
-    <View>
+    <ScrollView>
       <NewLectureScreen />
-    </View>
+      {/* <View
+        style={{
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.8,
+          shadowRadius: 2,
+          elevation: 1,
+          padding: 1,
+          marginLeft: 18,
+          marginRight: 18,
+          marginTop: 8
+        }}
+      /> */}
+      <View
+        style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 0.5,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.8,
+          shadowRadius: 2,
+          elevation: 2,
+          marginLeft: 18,
+          marginRight: 18,
+          marginTop: 8,
+        }}
+      />
+
+      <RecommendationLectureScreen />
+      <View
+        style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 0.5,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.8,
+          shadowRadius: 2,
+          elevation: 2,
+          marginLeft: 18,
+          marginRight: 18,
+          marginTop: 8,
+        }}
+      />
+      <PopularLectureScreen />
+    </ScrollView>
   );
 }
