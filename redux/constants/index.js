b26e43db4cb7f981561a9452442850472b2b5f99
hyejin <nhj1043@naver.com>
2020-08-07 13:52:32 +0900

    Refactoring: user 고유 id를 담은 Reducer를 삭제했습니다.
    - 요청할 때 jwt token을 그대로 헤더에 담아서 요청할 예정
    - jwt token 안에 user id의 정보가 담겨져 있어 따로 데이터를 담을 필요가
    없음

--- redux/constants/index.js
+++ redux/constants/index.js
@@ -1,4 +1,3 @@
-export const SET_USERID = 'SET_USERID';
 export const SET_EMAIL = 'SET_EMAIL';
 export const SET_NAME = 'SET_NAME';
 export const SET_TOKEN = 'SET_TOKEN';
--- redux/reducers/userReducer.js
+++ redux/reducers/userReducer.js
@@ -1,10 +1,9 @@
 import {
-  SET_USERID, SET_EMAIL, SET_NAME, SET_TOKEN, SET_LOGIN, SET_POSITION,
+  SET_EMAIL, SET_NAME, SET_TOKEN, SET_LOGIN, SET_POSITION,
 } from '../constants';
 
 // 기본값을 설정해주고
 const initialState = {
-  userId: undefined,
   email: '',
   name: '',
   token: '',
@@ -14,11 +13,9 @@ const initialState = {
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    // 위에서 작성한것처럼 type이 'SET_USERID'로 요청이 오면
-    case SET_USERID:
-      // 그밖의 state 값들을 포함하고 userId 라는 값에 actions에서 받은 userId 값을 Set 합니다.
-      return { ...state, userId: action.userId };
+    // 위에서 작성한것처럼 type이 'SET_EMAIL'로 요청이 오면
     case SET_EMAIL:
+      // 그밖의 state 값들을 포함하고 email 라는 값에 actions에서 받은 email 값을 Set 합니다.
       return { ...state, email: action.email };
     case SET_NAME:
       return { ...state, name: action.name };
