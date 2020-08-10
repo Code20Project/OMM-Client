6dc9dc3d825b4b7c8469059f8f931e3747a60155
hyejin <nhj1043@naver.com>
2020-08-10 19:08:34 +0900

    Feat: PATCH API를 추가했습니다.
    - profile patch api test 할 예정

--- connect/index.js
+++ connect/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   mentor: {
     get: (url) => fetch(`http://14.50.138.127:3001/mentor${url}`)
-      .then((res) => res.json()),
+      .then((res) => res.json()).catch((err) => console.log(`mentor GET ERR: ${err}`)),
     post: (url, body) => fetch(`http://14.50.138.127:3001/mentor${url}`, {
       method: 'POST',
       body: JSON.stringify(body),
@@ -19,9 +19,25 @@ module.exports = {
         }
       // 그 이외의 status 들은 클라이언트 쪽에서 undefined
       })
-      .catch((err) => {
-        console.log('err: ', err);
-      }),
+      .catch((err) => console.log(`mentor POST ERR: ${err}`)),
+    patch: (url, body) => fetch(`http://14.50.138.127:3001/mentor${url}`, {
+      method: 'PATCH',
+      body: JSON.stringify(body),
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (res.status === 200) {
+        if (res.status === 200) {
+          return res.json();
+        }
+        if (res.status === 201) {
+          return res.text();
+        }
+      }
+    })
+      .catch((err) => console.log(`mentor GET ERR: ${err}`)),
   },
   mentee: {
     get: (url) => fetch(`http://14.50.138.127:3001/mentee${url}`)
@@ -33,18 +49,35 @@ module.exports = {
       headers: {
         'Content-Type': 'application/json',
       },
+    }).then((res) => {
+      if (res.status === 200) {
+        return res.json();
+      }
+      if (res.status === 201) {
+        return res.text();
+      }
+      // 그 이외의 status 들은 클라이언트 쪽에서 undefined
     })
-      .then((res) => {
+      .catch((err) => {
+        console.log('err: ', err);
+      }),
+    patch: (url, body) => fetch(`http://14.50.138.127:3001/mentor${url}`, {
+      method: 'PATCH',
+      body: JSON.stringify(body),
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (res.status === 200) {
         if (res.status === 200) {
           return res.json();
         }
         if (res.status === 201) {
           return res.text();
         }
-        // 그 이외의 status 들은 클라이언트 쪽에서 undefined
-      })
-      .catch((err) => {
-        console.log('err: ', err);
-      }),
+      }
+    })
+      .catch((err) => console.log(`mentor GET ERR: ${err}`)),
   },
 };
